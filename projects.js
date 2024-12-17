d30745938f405f3fc918db309392aa04382a1d00
nbened <nickbenedetto11@gmail.com>
2024-12-17 16:13:46 -0600

    working portfolio

---
+++ projects.js
@@ -0,0 +1,23 @@
+const projectsData = [
+    {
+        title: "Automaticshorts",
+        description: "Generate dozens of videos just from a line of text.",
+        link: "https://www.automaticshorts.com/"
+    },
+    {
+        title: "TranslateSpace",
+        description: "Make your app multilingual in minutes.",
+        link: "https://www.translatespace.com/"
+    },
+    
+    {
+        title: "ChatGPT Smartwatch",
+        description: "World's first embedded AI agent on the wrist.",
+        link: "https://www.youtube.com/watch?v=ZCVDDcTL8xQ"
+    },
+    {
+        title: "Optic",
+        description: "Computer vision decryption glasses from scratch.",
+        link: "https://www.youtube.com/watch?v=vlioil_30t8"
+    }
+]; 
---
+++ ventures.js
@@ -0,0 +1,15 @@
+const ventures = [
+    {
+        title: "Automaticshorts",
+        link: "https://automaticshorts.com/",
+        dateRange: "Mar 2024 - Present",
+        subtitle: "Generate dozens of videos just from a line of text."
+    },
+    {
+        title: "Accent Engineering",
+        link: "",
+        dateRange: "Dec 2023 - Feb 2024",
+        subtitle: "Consulting firm for development and distribution."
+    }
+    // Add more ventures as needed
+];
